334d34b2fa5dab968c8836cf0f9d9c2df9acff8b
GoodMan28 <abhineetanand91@gmail.com>
2025-03-24 12:14:55 +0000

    new

--- week-2/week-2-async-js/easy/2-counter.js
+++ week-2/week-2-async-js/easy/2-counter.js
@@ -2,6 +2,7 @@ let count = 1;
 let limit = 15;
 function callBack(lt) {
     if(count <= lt) {
+        console.clear();
         console.log(count);
         count++;
         setTimeout(callBack, 1000, lt)
